de38286cf10bd11afbe7aa7e2797f98a20723278
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2022-09-12 15:41:45 -0500

    Test Push

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,6 +1,17 @@
 import io.javalin.Javalin
 
+//fun main() {
+//    val app = Javalin.create().start(7070)
+//    app.get("/") { ctx -> ctx.result("Hello World") }
+//}
+
 fun main() {
     val app = Javalin.create().start(7070)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    val data: String = "hi"
+    app.get("/") {
+        ctx -> ctx.result("Value: $data")
+    }
+    app.get("/path") {
+            ctx -> ctx.result("Other Path")
+    }
 }
