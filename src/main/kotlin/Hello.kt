ed5802308da01f4e79680583658d17d7f5f3def4
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2022-09-14 15:16:09 -0500

    Adds store and fetch api.

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,17 +1,24 @@
 import io.javalin.Javalin
 
-//fun main() {
-//    val app = Javalin.create().start(7070)
-//    app.get("/") { ctx -> ctx.result("Hello World") }
-//}
-
 fun main() {
     val app = Javalin.create().start(7070)
-    val data: String = "hi"
+
     app.get("/") {
-        ctx -> ctx.result("Value: $data")
+            ctx -> ctx.result("Commands:\n" +
+            "/hello_world: Prints 'hello world'.\n" +
+            "/store/{key}: Stores the 'key'.\n" +
+            "/fetch/{key}: Fetch the 'key'.")
     }
-    app.get("/path") {
-            ctx -> ctx.result("Other Path")
+
+    app.get("/hello_world") {
+        ctx -> ctx.result("Hello World!")
+    }
+
+    app.get("/store/{key}") {
+            ctx -> ctx.result("Storing: " + ctx.pathParam("key"))
+    }
+
+    app.get("/fetch/{key}") {
+            ctx -> ctx.result("Fetching: " + ctx.pathParam("key"))
     }
 }
