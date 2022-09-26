ee9aaa4b75b9e0148d3907e8fb0e1a24a1edaede
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2022-09-26 15:50:57 -0500

    Implement overarching class to hold system-wide datatypes.

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,20 @@
+/*
+ * Defines project-wide data types.
+ */
+
+/**
+ * Representation for different machine nodes.
+ */
+typealias NodeId = Int
+
+/**
+ * Represents key-value pair, where the key is a concatenation of the key and version.
+ * Class is used to print a representation of data received from the user.
+ */
+data class KeyValueReply(val key: String?, val value: String?)
+
+/**
+ * Represents key-version pair, where the key is a string and the version is an integer.
+ * This data class is our base representation of a "key" for the data
+ */
+data class KeyVersionPair(val key: String, val version: Int)
