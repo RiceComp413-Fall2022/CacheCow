a71bf1c87020819ca232187e193232925997a169
Mark Pepperl <marcuspepperl@gmail.com>
2022-10-17 10:25:12 -0500

    Improved project configs

---
+++ monitor-node/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Dashboard } from './components/Dashboard';
+
+export function App() {
+  return (
+    <div id='chart'>
+      <Dashboard />
+    </div>
+  );
+}
