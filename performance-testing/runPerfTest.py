a756a8d129e120712a764e72bc7cd70908de25d6
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2022-11-07 21:56:17 -0600

    Performance Testing Implementation

---
+++ performance-testing/runPerfTest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# This python script runs various performance tests for the CacheCow distributed
+# memory cache. It provides a standardized way to measure the performance of
+# different implementations.
+
+from functools import reduce
+import json
+from multiprocessing import Pool
+from optparse import OptionParser
+import requests
+
+from perfTests import *
+
+# Types of perfTestFunc: serial or parallel
+def serialPerfTest(function, data, reduction=lambda a, b: a + b):
+    return reduce(reduction, map(function, data))
+
+def parallelPerfTest(function, data, reduction=lambda a, b: a + b):
+    with Pool() as pool:
+      return reduce(reduction, pool.map(function, data))
+
+
+if __name__ == "__main__":
+    # Argument Parsing
+    parser = OptionParser(usage="perfTest.py -u node_url [options]")
+    parser.add_option("-u", "--url",
+                type="string",
+                dest="url",
+                help="Node URL for sending HTTP requests.")
+    parser.add_option("-p", "--parallel",
+                action = "store_true",
+                default=False,
+                dest="parallel",
+                help="Run Performance Tests in Parallel. " +
+                "By default, performance tests run in serial.")
+    parser.add_option("-b", "--time_backend",
+                action = "store_true",
+                default=False,
+                dest="time_backend",
+                help="Prints Backend Runtime.")
+    parser.add_option("-c", "--time_client",
+                action = "store_true",
+                default=False,
+                dest="time_client",
+                help="Prints Client Runtime. " +
+                "Defaults to client runtime if no other timing options are specified.")
+    parser.add_option("-s", "--time_script",
+                action = "store_true",
+                default=False,
+                dest="time_script",
+                help="Prints Performance Script Runtime.")
+    (options, args) = parser.parse_args()
+
+    if not options.url:
+        raise Exception("URL Argument is necessary for performance testing.")
+    if not options.time_backend and not options.time_script:
+        options.time_client = True
+
+    if options.time_script:
+        script_time = time.perf_counter()
+
+    # Run performance tests
+    perfTestFunc = serialPerfTest
+    if options.parallel:
+        perfTestFunc = parallelPerfTest
+    client_time = PerfTest.store_and_fetch_test(options.url, perfTestFunc=perfTestFunc)
+
+    # Print Performance Time Metrics
+    if options.time_backend:
+        # TODO: Support backend timing
+        fetched_data = json.loads(requests.get(url=f'http://{options.url}/v1/node-info').content)
+        print("Total Backend Timing: [Backend timing is not yet supported.]", fetched_data['nodeId'])
+    if options.time_client:
+        print("Total Client Timing: ", client_time)
+    if options.time_script:
+        print("Performance Script Timing: ", time.perf_counter() - script_time)
