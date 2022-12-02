95612c40061feb15b7b0a882e0d36c73fac41c4c
Mark Pepperl <56239439+marcuspepperl@users.noreply.github.com>
2022-12-02 10:50:19 -0600

    Update doc comments

--- cache-node/src/main/kotlin/launcher/INodeLauncher.kt
+++ cache-node/src/main/kotlin/launcher/INodeLauncher.kt
@@ -3,8 +3,14 @@ package launcher
 import NodeId
 
 /**
- * Interface
+ * Interface specifying module that can launh an additional node.
  */
 interface INodeLauncher {
+    
+    /**
+     * Launches a new node with the given node id.
+     *
+     * @param nodeId of node to be laumnched
+     */
     fun launchNode(nodeId: NodeId)
 }
